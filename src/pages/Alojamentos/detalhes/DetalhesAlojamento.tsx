cfed0db8bbc2a81f37ff772e35510ec4ba98d58a
Waphzz <miguelmelo70@gmail.com>
2024-01-11 20:50:30 +0000

    Sua mensagem de commit 6

--- src/pages/Alojamentos/detalhes/DetalhesAlojamento.tsx
+++ src/pages/Alojamentos/detalhes/DetalhesAlojamento.tsx
@@ -3,13 +3,13 @@ import { useParams, useNavigate } from 'react-router-dom';
 import db from '../../../firebaseConfig';
 import { doc, getDoc, collection, query, where, getDocs } from 'firebase/firestore';
 import { Alojamentos as AlojamentosModel } from '../../../models/Alojamentos';
-import { Reservas as ReservasModel } from '../../../models/Reservas';
+import { Reviews as ReviewsModel } from '../../../models/Reviews';
 
 import './DetalhesAlojamento.scss';
 
 const DetalhesAlojamento = () => {
   const [alojamento, setAlojamento] = useState<AlojamentosModel | null>(null);
-  const [reservas, setReservas] = useState<ReservasModel[]>([]);
+  const [reviews, setReviews] = useState<ReviewsModel[]>([]);
   const { id } = useParams<{ id: string }>();
   const navigate = useNavigate();
 
@@ -21,17 +21,17 @@ const DetalhesAlojamento = () => {
 
         if (docSnap.exists()) {
           setAlojamento(docSnap.data() as AlojamentosModel);
-          fetchReservas(id);
+          fetchReviews(id);
         } else {
           console.log('Nenhum alojamento encontrado!');
         }
       }
     };
 
-    const fetchReservas = async (alojamentoId: string) => {
-      const q = query(collection(db, 'reservas'), where('IDAlojamento', '==', alojamentoId));
+    const fetchReviews = async (alojamentoId: string) => {
+      const q = query(collection(db, 'reviews'), where('IDAlojamento', '==', alojamentoId));
       const querySnapshot = await getDocs(q);
-      setReservas(querySnapshot.docs.map(doc => doc.data() as ReservasModel));
+      setReviews(querySnapshot.docs.map(doc => doc.data() as ReviewsModel));
     };
 
     fetchAlojamento();
@@ -52,16 +52,16 @@ const DetalhesAlojamento = () => {
         <div className="info-alojamento">
           <h1>{alojamento.Nome}</h1>
           <div className="reviews">
-            {reservas.map(reserva => (
-              <p key={reserva.IDReserva}>{reserva.Notas}</p>
+            {reviews.map(review => (
+              <p key={review.IDReview}>{review.Comentário}</p>
             ))}
           </div>
         </div>
       </div>
       <div className="caracteristicas-alojamento">
-        <p>{alojamento.Morada}</p>
-        <p>{alojamento.Descrição}</p>
-        {/* Renderizar outras características aqui */}
+        <p>Morada: {alojamento.Morada}</p>
+        <p>Descrição: {alojamento.Descrição}</p>
+        {/* Adicione aqui outras características do alojamento */}
         <button onClick={handleEditClick} className="botao-editar">Editar</button>
       </div>
     </div>
